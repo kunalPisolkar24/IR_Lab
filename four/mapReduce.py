a56f159ca5cc4338bad06be04946157024e2fff0
kunalPisolkar24 <kpisolkar24@gmail.com>
2024-09-30 19:00:08 +0530

    added 4th practical

---
+++ four/mapReduce.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+# Mapper function
+def mapper(line):
+    line = line.lower()
+    
+    for char in line:
+        if char.isalpha():
+            yield (char, 1)
+
+# Reducer function
+def reducer(key, values):
+    return (key, sum(values))
+
+# MapReduce simulation
+def map_reduce(input_data):
+    intermediate = defaultdict(list)
+    for line in input_data:
+        for key, value in mapper(line):
+            intermediate[key].append(value)
+    
+    results = []
+    for key in intermediate:
+        results.append(reducer(key, intermediate[key]))
+    
+    return results
+
+# Function to read data from a text file and process it
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    output = map_reduce(lines)
+    
+    for char, count in output:
+        print(f"({char}: {count})")
+
+file_path = '/home/kunalpisolkar/Documents/Practicals/IR_Lab/four/document1.txt'  
+process_file(file_path)
